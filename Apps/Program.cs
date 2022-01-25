ad6fb9a0c3d159d5533927e2ab4cf6a9e2462125
Kaan Yarayan <rknyryn@gmail.com>
2022-01-25 16:31:51 +0300

    first commit

---
+++ Apps/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+
+//ARRAY
+#region
+//var arr = new DataStructures.Array.Array<int>();
+//arr.Add(3);
+//arr.Add(4);
+//arr.Add(5);
+//arr.Add(6);
+//arr.Add(7);
+//arr.Remove();
+//arr.Remove();
+//arr.Remove();
+//arr.Remove();
+
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item);
+//    Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+//}
+
+//Console.WriteLine("--------------------");
+
+//arr.Remove(3);
+//arr.Remove(4);
+//arr.Remove(7);
+
+//Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+//arr.Where(x => x % 2 == 0).ToList().ForEach(x => Console.WriteLine(x));
+#endregion
+
+#region
+//var arr2 = new DataStructures.Array.Array<int>(1, 2, 3, 4, 12);
+//foreach (var item in arr2)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine("--------------------");
+
+//var p1 = new DataStructures.Array.Array<int> { 1, 2, 23, 89 };
+//var p2 = new int[] { 14, 25, 23, 75 };
+//var p3 = new List<int>() { 5, 15, 20, 25 };
+//var p4 = new ArrayList() { 98, 97, 96, 95 };
+
+//var arr3 = new DataStructures.Array.Array<int>(p3);
+//foreach (var item in arr3)
+//    Console.WriteLine(item);
+
+//Console.WriteLine("--------------------");
+#endregion
+
+#region
+//var arr4 = new DataStructures.Array.Array<int>();
+//for (int i = 0; i < 9; i++)
+//{
+//    arr4.Add(i + 1);
+//    Console.WriteLine($"{i + 1} has been added to array");
+//    Console.WriteLine($"{arr4.Count}/{arr4.Capacity}");
+//}
+//Console.WriteLine("---");
+//for (int i = arr4.Count - 1; i >= 1; i--)
+//{
+//    Console.WriteLine($"{arr4.Remove()} has been removed to array");
+//    Console.WriteLine($"{arr4.Count}/{arr4.Capacity}");
+//}
+#endregion
+
+Console.ReadKey();
---
+++ DataStructures/Array/Array.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Array
+{
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] InnerList;
+        public int Count { get; set; }
+        public int Capacity => InnerList.Length;
+
+        public Array()
+        {
+            InnerList = new T[2];
+            Count = 0;
+        }
+        public Array(params T[] initial)
+        {
+            InnerList = new T[initial.Length];
+            Count = 0;
+            foreach (var item in initial)
+                Add(item);
+        }
+        public Array(IEnumerable<T> collection)
+        {
+            InnerList = new T[collection.Count()];
+            Count = 0;
+            foreach (var item in collection)
+                Add(item);
+        }
+        public object Clone()
+        {
+            ///return this.MemberwiseClone();
+            var arr = new Array<T>();
+            foreach (var item in this)
+                arr.Add(item);
+            return arr;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InnerList.Take(Count).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void Add(T item)
+        {
+            if (InnerList.Length == Count)
+                DoubleArray();
+            InnerList[Count] = item;
+            Count++;
+        }
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+                Add(item);
+        }
+        public T Remove()
+        {
+            if (Count == 0)
+                throw new Exception("There is no more item to be removod from the array.");
+
+            var temp = InnerList[Count - 1];
+            if (Count > 0)
+                Count--;
+
+            if (InnerList.Length / 4 == Count)
+                HalfArray();
+            return temp;
+        }
+        public bool Remove(T item)
+        {
+            if (Count == 0)
+                throw new Exception("There is no more item to be removod from the array.");
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (InnerList[i].Equals(item))
+                {
+                    Swap(i);
+                    Count--;
+
+                    if (InnerList.Length / 4 == Count)
+                        HalfArray();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Swap(int index)
+        {
+            for (int i = index; i < Count-1; i++)
+            {
+                InnerList[i] = InnerList[i+1];
+            }
+        }
+
+        private void HalfArray()
+        {
+            if (InnerList.Length > 2)
+            {
+                var temp = new T[InnerList.Length / 2];
+                System.Array.Copy(InnerList, temp, temp.Length);
+                InnerList = temp;
+            }
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new T[InnerList.Length * 2];
+            System.Array.Copy(InnerList, temp, InnerList.Length);
+            InnerList = temp;
+        }
+    }
+}
